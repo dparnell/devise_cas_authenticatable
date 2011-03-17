b3cf8c00a9343e24066349f40b8c2f60c624f993
Daniel Parnell <me@danielparnell.com>
2011-03-18 10:54:13 +1100

    Fix for running inside tomcat

--- lib/devise_cas_authenticatable/session.rb
+++ lib/devise_cas_authenticatable/session.rb
@@ -2,6 +2,10 @@ module RubyCas
   class Session < ActiveRecord::SessionStore::Session
     before_save :save_cas_ticket
     
+    def self.find_by_session_id(session_id)
+      find :first, :conditions => {:session_id=>session_id}
+    end
+    
     def save_cas_ticket
       if self.data && self.cas_ticket.nil?
         t = self.data['cas_last_valid_ticket']
